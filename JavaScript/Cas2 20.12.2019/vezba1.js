47e1e9fbf80d3e7776891cc08291a6e59e0c1996
IvanovskaD <58231196+IvanovskaD@users.noreply.github.com>
2019-12-26 21:55:21 +0100

    Add files via upload

---
+++ JavaScript/Cas2 20.12.2019/vezba1.js
@@ -0,0 +1,51 @@
+// stringObj = new String('Hello');
+// console.log(stringObj.lenght);
+// console.log(stringObj.substring(2,4))
+// console.log(stringObj.charAt(0))
+
+// pi = Math.PI
+// e = Math.E
+// sin = Math.sin(1.56)
+// log= Math.log(10)
+// round = Math.round (10.10004020500)
+
+// const symbol1 = Symbol();
+// const symbol2 = Symbol(42);
+// const symbol3 = Symbol ('foo');
+
+// console.log(typeof symbol1);
+// console.log(symbol3.toString());
+// console.log(Symbol('foo')) === Symbol('foo');
+
+// var x = 2
+// var y = 39
+// var z = "42"
+// console.log(eval("x + y + 1"))
+// console.log(eval(z))
+
+// today = new Date ()
+// birthday = new Date ("December 17, 1995 03:24:00")
+// birthday = new Date (95,12,17)
+// birthday = new Date (95,12,17,3,24,0)
+// console.log(birthday)
+
+// var upperText="ALPHABET"
+// upperText.toLowerCase()
+// var lowerText = "alphabet"
+// lowerText.toUpperCase()
+
+// console.log(lowerText.toUpperCase)
+
+
+var x = 5;
+var y = 526;
+var z = x--;
+console.log(z)
+console.log(x+y-y)
+console.log(y/2 + x/2 + x+y - y)
+console.log (x==y)
+console.log(y-=226)
+console.log(x>y)
+console.log(y>=z)
+console.log(x-=3 + y/x / y)
+
---
+++ JavaScript/Cas4 26.12.2019/vezba1.js
@@ -0,0 +1,82 @@
+
+// // var i;
+// // for (i=0; i<10; i++){
+
+// // 	console.log(i)
+// }
+
+
+// var i;
+// for(i=0; i<15; i++){
+// 	console.log("Brojot e " + i + " momentalno");
+// 	console.log(1);
+// }
+
+// var avtomobili = ""
+// var lista = ["BMW", "AUDI", "VW"]
+// var len = lista.lenght
+// var i = 0;
+// for (;i<len;){
+// 	avtomobili += lista[i] + " ";
+// 	i++;
+// }
+
+
+// var avtomobili = ""
+// var lista = ["BMW", "AUDI", "VW"]
+// for (x of lista){
+// 	avtomobili += x + " ";
+// 	console.log(x)
+// 	// console.log(avtomobili)
+// }
+
+
+// var rezultat = ""
+// var lista = ["x","y","z","v","h","b","a","l","d","c","e"]
+// for (x of lista){
+
+// 	rezultat += x + " ";
+// 	console.log(x)
+// }
+
+// console.log(rezultat)
+
+// var rezultat = ""
+// var lista = ["x","y","z","v","h","b","a","l","d","c","e"]
+// for(i=0;i<=10;i++) {
+// 	console.log(lista[i])
+// 	rezultat += lista[i] + " ";
+	
+// }
+// console.log(rezultat)
+
+
+
+// var text = "";
+// var i = 0;
+
+// do{
+// 	text += "Brojot e " + i + " ";
+// 	i++
+// 	console.log(text)
+// }
+
+// while(i<=5);
+// console.log(text);
+
+
+var rezultat = ""
+var niza = ["x","y","z","v","h","b","a","l","d","c","e"]
+for (i=0;i<=10;i++){
+	if (x=i+2) {
+		rezultat = x + " "
+		console.log(re)
+	}
+}
+
+
+
+
+
+
+
